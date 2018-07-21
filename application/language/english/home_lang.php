5c683c4e7d7c422c55e9773a80840a8affa0015b
Carlos Souza <contato@carlossouza.com>
2018-07-21 19:32:42 +0100

    site test
    site teste

---
+++ application/language/english/home_lang.php
@@ -0,0 +1,131 @@
+<?php
+$lang["lang"] = 'en';
+$lang["description"] = "I am Carlos Henrique de Sales Souza, 26, a native of Rio de Janeiro - Brazil. I schedule more than 10 years, focusing on web development.";
+$lang["keyword"] = "php, html, portifolio, system analyst, ecommerce, carlossouza, carlos, developer, web, freelancer, javascript, mysql, web developer";
+$lang["linkLanguage"] = "<a href='http://carlossouza.com/portifolio/'> <img src='http://carlossouza.com/portifolio/assets/img/icons/Brazil.png' height='15' width='15' style='margin-bottom: 3px' alt='Portuguese Version'> </a>";
+$lang["title"] = 'carlossouza.com - Welcome';
+$lang["name"] = 'carlossouza.com';
+$lang["services"] = 'services';
+$lang["portfolio"] = 'portfolio';
+$lang["about"] = 'about';
+$lang["social"] = 'social';
+$lang["contact"] = 'contact';
+$lang["thanks"] = 'Thanks for the visit!';
+$lang["welcome"] = 'Make yourself at home';
+$lang["takealook"] = 'Look more';
+$lang["servicesDescription"] = 'Some of the services that can help your product even better';
+$lang["servicesEcommerce"] = 'E-commerce';
+$lang["servicesEcommerceDescription"] = 'I have many success stories in this area, including the largest e-commerce in Latin America.';
+$lang["servicesResponsive"] = 'Responsive Design';
+$lang["servicesResponsiveDescription"] = 'Nothing better than having your product on all devices, and better, totally adapted to it!';
+$lang["salesGrowing"] = 'Demand growth';
+$lang["salesGrowingDescription"] = 'What matters have a great analyst, if he can not understand the requested demand? Therefore its growth is stopped!';
+$lang["portfolioDescription"] = 'Here are some products that I have ever attended';
+$lang["aboutDescription"] = 'A short summary of my life';
+$lang["socialDescription"] = 'Find me on the web!';
+$lang["descriptionCarlos"] = 'contato@carlossouza.com';
+$lang["socialDescription"] = 'Leave a comment whenever you can and want to, questions, criticism and praise are also accepted: D';
+$lang["contactDescription"] = 'Email me now ...';
+$lang["yourName"] = 'Your Name';
+$lang["yourEmail"] = 'Your Email';
+$lang["yourPhone"] = 'Your Phone';
+$lang["yourDescription"] = 'Your Description';
+$lang["yourNameDescription"] = 'Please enter your name';
+$lang["yourEmailDescription"] = 'Please enter your email address';
+$lang["yourPhoneDescription"] = 'Please enter your phone number';
+$lang["yourMessageDescription"] = 'Please enter a message';
+$lang["sendMessage"] = 'SEND MESSAGE';
+$lang["privacy"] = 'privacy';
+$lang["terms"] = 'terms';
+
+$lang["thisIs"] = 'This is';
+$lang["justThe"] = 'Just the ';
+$lang["beginning"] = 'beginning...';
+
+$lang["year1988"] = 'march 31 1988';
+$lang["year1988Title"] = 'Hello World! :D';
+$lang["year1988Description"] = 'I was born at the end of the day, family all gathered, so I grew up ....';
+
+$lang["year2004"] = 'Mid 2004';
+$lang["year2004Title"] = 'What I wanted';
+$lang["year2004Description"] = 'My first contact with development! It was with the Visual Basic language, and from that moment, I knew what I wanted for my life.';
+
+$lang["year2006"] = 'Early 2006';
+$lang["year2006Title"] = 'Freshman at UERJ';
+$lang["year2006Description"] = 'Skip to one of the best colleges in the area, helped me to gain more knowledge, learning a lot from the room of friends and teachers';
+
+$lang["year2007"] = 'The Year 2007';
+$lang["year2007Title"] = 'First project and First job in IT';
+$lang["year2007Description"] = "Coordinated by Professor <a href='https://www.linkedin.com/profile/view?id=154028615' target='_blank'> Alexander Rojas </a> participated through scientific research, the 'Karioka' project,
+Which had the idea to make an operating system for the academic sphere. <br>
+Another important point was that this year (in July), I got my first job in <a href='http://www.hstern.net/site/home/default.asp' target='_blank'> H. Stern </a> as support intern";
+
+$lang["year2008"] = 'The Year 2008';
+$lang["year2008Title"] = 'First job developing';
+$lang["year2008Description"] = "In early 2008 I was promoted in H. Stern, but he saw that something was missing in my life ... a part of the missing met 
+ even in this company! Then my girlfriend <a href='https://www.facebook.com/juliana.d.costa.75' target='_blank'> Juliana </a>, which has since
+ is with me today. The other part was looking in the selection process to work development intern at 'OI'. Anyway .... I got it!
+ I started working with development, and really learn to program, modeling databases and raise requirements! : D";
+
+$lang["year2009"] = 'The Year 2009';
+$lang["year2009Title"] = 'Improving developing';
+$lang["year2009Description"] = "As intern in 'OI', my schedule was from 07:00 to 14:00, but it was hard the day does not go out after 17:00 until 22:00 ... already out
+ because I was hungry for knowledge, and loving out everything those 'new'! <br>
+ In five months, I felt safe to try a spot as an employee, and the Netmaker was this company that believed in me! The Netmaker was a company that
+ provided services for public agencies, I was on a project in <a href='http://portal.fiocruz.br/pt-br' target='_blank'> FIOCRUZ </a>. <br>
+ In just over a year that I was there, I made dozens of sites and systems met the object-oriented programming, design patterns and a greater zeal for the code.";
+
+$lang["year2010"] = 'The Year 2010';
+$lang["year2010Title"] = 'First Post on Facebook API in Brazil';
+$lang["year2010Description"] = "At this time, already enjoyed sharing experiences with the IT community, and started using a blog to it. In parallel,
+ I went to Orbiweb like the challenge of making games on the web! (a dream for a 21 years), this time studied a lot Action Script, SmartFoxServer and 
+ integrations with Facebook, making the first <a href='http://carlossouza.com/wp/integrando-api-of-facebook-to-site' target='_blank'> post </a> in Portuguese on the subject.
+ I could have the pleasure of leading the Orbitweb the development team also at this time.
+";
+
+$lang["year2011"] = 'The Year 2011';
+$lang["year2011Title"] = "Let's get married love?";
+$lang["year2011Description"] = "A friend from college invited me to work on <a href='http://www.acotel.com/' target='_blank'> Acotel </a>, and the proximity of my house,
+ did not think twice to work for this company. <br>
+ Met the mobile market in depth, working on various projects of <a href='http://www.tim.com.br/' target='_blank'> </a> TIM, the 
+ <a href='http://grupoabril.com.br/' target='_blank'> Group Abril </a> and <a href ='http://esporteinterativo.com.br/' target='_blank'> Esporte Interativo </a>. <br>
+ Later this year, I married Juliana, and I'm sure that was one of the best decisions of my life! : D
+       ";
+
+$lang["year2012"] = 'The Year 2012';
+$lang["year2012Title"] = "I'll be agile! I will study more!";
+$lang["year2012Description"] = "At this stage, I began to read and study a lot about best practices for development, and met agile methodologies!
+ I did two courses on the subject, and now apply the concept in my personal life. <br>
+ Parallel to this, the same friend who took me to Acotel, me
+ indicated a company that was looking for JAVA developers, <a href='http://www.quality.com.br/' target='_blank'> Quality Software </a>.
+ I could delve into SOA thoroughly, making great additions to <a href='http://www.capesesp.com.br/' target='_blank'> CAPESESP </a>.
+";
+
+$lang["year2013"] = 'The Year 2013';
+$lang["year2013Title"] = 'My daughter was born!';
+$lang["year2013Description"] = "With a good experience in integrations, went to the <a href='http://www.hotelurbano.com/' target='_blank'> Hotel Urbano </a>
+ With the initial challenge of making large additions to the site, were made dozens! All reaching expectations, integrations that increased
+ Company portfolio of 40,000 hotels for 180,000 hotels, for example. Integrations with metasearches (TripAdvisor, Kayak, mundi ...) made
+ the site appears for those seeking hotel options, and consequently, sales increased considerably. <br>
+ At this time, the greatest gift of my life here! Weighing 3.195 kg and 50.0 cm, Luiza came into the world to change my life and that of my wife!
+ <br>
+ Today I can not imagine how my life would be without my little princess ...
+";
+
+$lang["year2014"] = 'The Year 2014';
+$lang["year2014Title"] = 'Greatest Hits goals';
+$lang["year2014Description"] = "With the success in integration, the board of Hotel Urbano, decided after three years on the market and managing to become the largest OTA in Brazil,
+ change the shape of your product, which would result in an overall restructuring of the company's business model (changing from the framework, to the server hosting). The term?
+ <b> 90 days </b>. Meta very aggressive, but after a few days without sleep, and several hours programming, in <b> 45 days </b> the project was to go live, in just doing
+ 3 months more than 15 million reais! In addition to the new form of business, the site became responsive, have greater control metrics and being praised by users.
+";
+
+
+$lang["year2015"] = 'The Year 2015';
+$lang["year2015Title"] = 'In the largest E-commerce Latin america';
+$lang["year2015Description"] = "I really like e-commerce, and want a better place in Brazil than the <a href='http://www.b2wdigital.com/' target='_blank'> B2W </a>? <br>
+ Simply's largest e-commerce in Latin America, being among the world's largest retail! <br>
+ Implementation of responsive sites, code versioning and implementation of the 'Customer Service area' are some of the cases I've had in two months from now.
+         ";
+
+?>
