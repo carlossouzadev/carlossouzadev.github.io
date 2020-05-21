797ac570982c5b0c7be506ac4deb3ed426d793c2
carlossouzadev <contato@carlossouza.com>
2020-05-21 21:42:02 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+      $response = array(
+          "outputSpeech" => array(
+              "type" => "PlainText",
+              "text" => "Hello World"
+          )
+      );
+
+      $data = array(
+              "version" => "0.1",
+              "sessionAttributes" => array(
+                  "countActionList" => array(
+                      "Read" => true,
+                      "category" => true
+                  )
+              ) ,
+              "response" => "Olá mundo",
+              "shouldEndSession" => false
+      );
+      echo json_encode($data);
+      die();
